ab46cb3506a8e9685cdb0edba4b6e28fe524b9c7
AathifSana <sanaya.aathif961008@gmail.com>
2020-01-26 18:36:10 +0530

    added save and load loadaframe from tsv methods to the DataSource

--- src/main/scala/datasources/DataSource.scala
+++ src/main/scala/datasources/DataSource.scala
@@ -2,7 +2,7 @@ package datasources
 
 import common.Environment
 import common.Constants._
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SaveMode}
 
 
 object DataSource {
@@ -18,5 +18,13 @@ object DataSource {
       .option("delimiter", delimiter)
       .option("header", header)
       .load(path)
+  }
+
+  def saveDataFrameAsTSV(df: DataFrame, path: String, delimiter: String = TAB, header: String = STR_BOOL_FALSE): Unit = {
+    df.write.mode(SaveMode.Overwrite)
+      .format(CSV_FORMAT)
+      .option("delimiter", delimiter)
+      .option("header", header)
+      .save(path)
   }
 }
