cefa3c82a65ad941c68511d457c2fea072db003d
AathifSana <sanaya.aathif961008@gmail.com>
2020-01-29 09:21:09 +0530

    added getting dataframe with schema method in datasource

--- src/main/scala/datasources/DataSource.scala
+++ src/main/scala/datasources/DataSource.scala
@@ -2,6 +2,7 @@ package datasources
 
 import common.Environment
 import common.Constants._
+import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SaveMode}
 
 
@@ -10,13 +11,24 @@ object DataSource {
   lazy val sparkSession = Environment.sparkSession
   import sparkSession.implicits._
 
-  def getDataFrameByTSV(path: String, delimiter: String = TAB, header: String = STR_BOOL_FALSE) : DataFrame = {
+  def getTSVDataFrame(path: String, delimiter: String = TAB, header: String = STR_BOOL_FALSE) : DataFrame = {
 
     sparkSession
       .read
       .format(CSV_FORMAT)
       .option("delimiter", delimiter)
       .option("header", header)
+      .load(path)
+  }
+
+  def getTSVDataFrameWithSchema(path: String, schema: StructType, delimiter: String = TAB) : DataFrame = {
+
+    sparkSession
+      .read
+      .schema(schema)
+      .format(CSV_FORMAT)
+      .option("delimiter", delimiter)
+      .option("header", STR_BOOL_FALSE)
       .load(path)
   }
 
