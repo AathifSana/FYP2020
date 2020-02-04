ee44b28ce0802fffb23b8dd9cd78fc8278253d74
AathifSana <sanaya.aathif961008@gmail.com>
2020-02-04 12:43:18 +0530

    Created FrequentlyBoughtJob

---
+++ src/main/scala/jobs/FrequentlyBoughtJob.scala
@@ -0,0 +1,91 @@
+package jobs
+
+import com.twitter.scalding.Args
+import common.Environment
+import datasources.DataSource
+import common.Constants._
+import org.apache.spark.ml.fpm.FPGrowth
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object FrequentlyBoughtJob {
+
+  def main(args: Array[String]): Unit = {
+
+    val params = Args(args)
+
+    implicit val sc = Environment.sparkSession.sparkContext
+    sc.setLogLevel("ERROR")
+
+    val inputPathSeg = params.getOrElse("inputSeg" , "")
+    val inputPath = params.required("input")
+    val writePath = params.required("output")
+    val minSupport = params.getOrElse("support" , "0.2").toDouble
+    val minConfidence = params.getOrElse("confidence" , "0.1").toDouble
+
+    val customerSegments = DataSource.getTSVDataFrame(inputPathSeg, header = STR_BOOL_TRUE)
+    val transactions = DataSource.getTSVDataFrameWithSchema(inputPath, schema)
+
+    val itemsPerPerchases = transactions
+      .groupBy(INVOICE_NO, DATE_TIME, CUSTOMER_ID, COUNTRY)
+      .agg(collect_set(STOCK_CODE) as ITEMS)
+
+
+    val fpgrowth = new FPGrowth().setItemsCol(ITEMS).setMinSupport(minSupport).setMinConfidence(minConfidence)
+    val model = fpgrowth.fit(transactions)
+
+    model.freqItemsets.show()
+    model.associationRules.show()
+
+  }
+
+  val INVOICE_NO = "InvoiceNo"
+  val STOCK_CODE = "StockCode"
+  val PRODUCT_NAME = "Description"
+  val QUANTITY = "Quantity"
+  val DATE_TIME = "InvoiceDate"
+  val PRICE = "UnitPrice"
+  val CUSTOMER_ID = "CustomerID"
+  val COUNTRY = "Country"
+  val ALL = "*"
+  val SEGMENT = "segment"
+  val ITEMS = "items"
+
+  //Per Perchase-----------------------
+  val PRICE_PER_PURCHASE = "pricePerPurchase"
+  val PRODUCT_QUANTITY_RATIO_PER_PURCHASE = "productQuantityRatioPerPurchase"
+
+
+  //Per Customer-----------------------
+  val PURCHASES_PER_CUSTOMER = "purchasesPerCustomer"
+  val AVG_PRICE_PER_PURCHASE_PER_CUSTOMER = "avgPricePerPurchasePerCustomer"
+  val AVG_PRODUCT_QUANTITY_RATIO_PER_PURCHASE_PER_CUSTOMER = "avgProductQuantityRatioPerPurchasePerCustomer"
+
+
+  //Avg -------------------------------
+  val AVG_PURCHASES_PER_CUSTOMER = "avgPurchasesPerCustomer"
+  val AVG_PRICE_PER_PURCHASE = "avgPricePerPurchase"
+  val AVG_PRODUCT_QUANTITY_RATIO_PER_PURCHASE = "avgProductQuantityRatioPerPurchase"
+
+
+  val TOTAL_REVENUE = "totalRevenue"
+  val TOTAL_PURCHASES = "totalPurchases"
+  val TOTAL_CUSTOMERS = "totalCustomers"
+
+  val NO_OF_CUSTOMERS = "noOfCustomers"
+
+  val schema = StructType(Array(
+    StructField(INVOICE_NO, StringType, true),
+    StructField(STOCK_CODE, StringType, true),
+    StructField(PRODUCT_NAME, StringType, true),
+    StructField(QUANTITY, IntegerType, true),
+    StructField(DATE_TIME, StringType, true),
+    StructField(PRICE, FloatType, true),
+    StructField(CUSTOMER_ID, StringType, true),
+    StructField(COUNTRY, StringType, true)
+  ))
+
+}
+
+
+
